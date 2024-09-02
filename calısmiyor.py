35ec6a1e69c27b153160a3053ec3e66acd9db168
A.Emre ELP <a.emreelp@gmail.com>
2024-09-02 16:32:29 +0300

    last version
    Exception in thread Thread-2 (db_worker):
    Traceback (most recent call last):
      File "/usr/lib/python3.11/threading.py", line 1038, in _bootstrap_inner
        self.run()
      File "/usr/lib/python3.11/threading.py", line 975, in run
        self._target(*self._args, **self._kwargs)
      File "/home/raspi1/Desktop/Rxinterrpt/DatabaseOperations/main.py", line 127, in db_worker
        insertToDB("BatteryManagement", "Cluster", {"data": data})
      File "/home/raspi1/Desktop/Rxinterrpt/DatabaseOperations/main.py", line 24, in insertToDB
        client = MongoClient(uri, server_api=ServerApi('1'))
                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/home/raspi1/myenv/lib/python3.11/site-packages/pymongo/mongo_client.py", line 774, in __init__
        res = uri_parser.parse_uri(
              ^^^^^^^^^^^^^^^^^^^^^
      File "/home/raspi1/myenv/lib/python3.11/site-packages/pymongo/uri_parser.py", line 543, in parse_uri
        dns_options = dns_resolver.get_options()
                      ^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/home/raspi1/myenv/lib/python3.11/site-packages/pymongo/srv_resolver.py", line 99, in get_options
        raise ConfigurationError(str(exc)) from None
    pymongo.errors.ConfigurationError: The resolution lifetime expired after 21.120 seconds: Server Do53:192.168.43.1@53 answered The DNS operation timed out.; Server Do53:192.168.43.1@53 answered The DNS operation timed out.; Server Do53:192.168.43.1@53 answered The DNS operation timed out.; Server Do53:192.168.43.1@53 answered The DNS operation timed out.; Server Do53:192.168.43.1@53 answered The DNS operation timed out.; Server Do53:192.168.43.1@53 answered The DNS operation timed out.; Server Do53:192.168.43.1@53 answered The DNS operation timed out.
    
    Errorr!!!!

---
+++ calısmiyor.py
@@ -0,0 +1,156 @@
+import serial
+import select
+import time
+import datetime
+import threading
+import concurrent.futures
+from collections import deque
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+"""Database operations is here"""
+testList = []
+crc = 0
+indexTrack = -1
+dn = 0
+sum = 0
+resultList = []
+storeData = deque()
+
+
+def insertToDB(database, collection, data):
+    uri = "mongodb+srv://ASSAN:sifre@batterymanagementcluste.wyc4v.mongodb.net/?retryWrites=true&w=majority&appName=BatteryManagementCluster"
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    db = client[f'{database}']
+    collection = db[f"{collection}"]
+
+    document = data
+    insert_doc = collection.insert_one(document)
+    client.close()
+
+
+def interpretation(byte):
+    global testList
+    global dataLenght
+    global crc
+    global indexTrack
+    global dn
+    global sum
+    global resultList
+
+    if str(byte) > "7F":
+        y = int(byte, 16) & 7
+        indexTrack = 0
+        testList.append(byte)
+        resultList.append(byte)
+        sum = sum + int(byte, 16)
+
+    elif indexTrack > 2:
+        if (indexTrack > int(dataLenght, 16) + 2):
+            crc = byte
+            print("Data Geldi")
+            dn = 0
+            indexTrack = -1
+            resultList.append(byte)
+            if (sum % 9 == int(byte, 16)):
+                print("Data True")
+                sum = 0
+                try:
+                    return resultList
+                except:
+                    print("interpretation Error")
+                finally:
+                    resultList = []
+            else:
+                print("Data False")
+                sum = 0
+                resultList = []
+        else:
+            testList.append(byte)
+            resultList.append(byte)
+            indexTrack = indexTrack + 1
+            dn = dn + 1
+            sum = sum + int(byte, 16)
+
+    elif indexTrack == 2:
+        dataLenght = byte
+        testList.append(byte)
+        resultList.append(byte)
+        indexTrack = 3
+        sum = sum + int(byte, 16)
+
+    elif indexTrack == 1:
+        komut = byte
+        testList.append(byte)
+        resultList.append(byte)
+        indexTrack = 2
+        sum = sum + int(byte, 16)
+
+    elif indexTrack == 0:
+        k = byte
+        n = ((0 * 128) + int(k, 16))
+        indexTrack = 1
+        testList.append(byte)
+        resultList.append(byte)
+        sum = sum + int(byte, 16)
+
+
+def storeOurDatas(data):
+    global storeData
+    storeData.append(data)
+
+
+def main():
+    paketNum = 0
+    my_list = []
+    ser = serial.Serial(
+        port="/dev/serial0",
+        baudrate=56000,
+        timeout=0
+    )
+    try:
+        while True:
+            if select.select([ser], [], [], 0)[0]:
+                data = ser.read_all()
+                if data:
+                    my_list.append(data.hex())
+
+            if (len(my_list) > 0):
+                for item in my_list:
+                    with concurrent.futures.ThreadPoolExecutor() as executor:
+                        a = executor.submit(interpretation, item)
+                        a = interpretation(item)
+                        my_list = []
+                        if (a != None):
+                            print(a[0])
+                            now = time.localtime(time.time())
+                            start = time.time()
+                            a.append(time.strftime("%y/%m/%d %H:%M", now))
+                            saltData = int(a[4], 16) * 100 + int(a[5], 16) * 10 + int(a[6], 16) + int(a[7],
+                                                                                                      16) * 0.1 + int(
+                                a[8], 16) * 0.10 + int(a[9], 16) * 0.001
+                            storeOurDatas(a)
+                            print("Store Data is printing")
+                            print(storeData)
+                            if storeData is not None:
+                                insertToDB("Yasin", "Emre",
+                                           {"header": storeData[0][0], "K constant": storeData[0][1],
+                                            "Command": storeData[0][2], "Data Length": storeData[0][3],
+                                            "Data": saltData, "CRC": storeData[0][10], "Time Now": storeData[0][11]})
+                                storeData.popleft()
+
+                            print(a)
+                            a = []
+                            print(a)
+                            end = time.time()
+                            print(end - start)
+                            print(threading.active_count())
+                            print(threading.enumerate())
+    except KeyboardInterrupt:
+        print("Exiting program.")
+    finally:
+        ser.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,160 @@
+import serial
+import select
+import time
+import datetime
+import threading
+import queue
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+# Setup MongoDB Client URI
+uri = "mongodb+srv://ASSAN:sifre@batterymanagementcluste.wyc4v.mongodb.net/?retryWrites=true&w=majority&appName=BatteryManagementCluster"
+
+# Thread-safe queue to hold data
+data_queue = queue.Queue()
+testList = []
+crc = 0
+indexTrack = -1
+dn = 0
+sum = 0
+resultList = []
+
+# MongoDB insert function
+def insertToDB(database, collection, data):
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db = client[database]
+    coll = db[collection]
+    coll.insert_one(data)
+    client.close()
+
+
+def interpretation(byte):
+    global testList
+    global dataLenght
+    global crc
+    global indexTrack
+    global dn
+    global sum
+    global resultList
+
+    if str(byte) > "7F":
+        y = int(byte, 16) & 7
+        indexTrack = 0
+        testList.append(byte)
+        resultList.append(byte)
+        sum = sum + int(byte, 16)
+
+    elif indexTrack > 2:
+        if (indexTrack > int(dataLenght, 16) + 2):
+            crc = byte
+            print("Data Geldi")
+            dn = 0
+            indexTrack = -1
+            resultList.append(byte)
+            if (sum % 9 == int(byte, 16)):
+                print("Data True")
+                sum = 0
+                try:
+                    return resultList
+                except:
+                    print("interpretation Error")
+                finally:
+                    resultList = []
+            else:
+                print("Data False")
+                sum = 0
+                resultList = []
+        else:
+            testList.append(byte)
+            resultList.append(byte)
+            indexTrack = indexTrack + 1
+            dn = dn + 1
+            sum = sum + int(byte, 16)
+
+    elif indexTrack == 2:
+        dataLenght = byte
+        testList.append(byte)
+        resultList.append(byte)
+        indexTrack = 3
+        sum = sum + int(byte, 16)
+
+    elif indexTrack == 1:
+        komut = byte
+        testList.append(byte)
+        resultList.append(byte)
+        indexTrack = 2
+        sum = sum + int(byte, 16)
+
+    elif indexTrack == 0:
+        k = byte
+        n = ((0 * 128) + int(k, 16))
+        indexTrack = 1
+        testList.append(byte)
+        resultList.append(byte)
+        sum = sum + int(byte, 16)
+
+
+# Function to read data from the serial port in a separate thread
+def read_serial(ser):
+    paketNum = 0
+    my_list = []
+    
+    while True:
+        if select.select([ser], [], [], 0)[0]:
+            # Use select to wait for data to be available
+            data = ser.read_all()
+            if data:
+                my_list.append(data.hex())
+                paketNum += 1
+
+        if my_list:
+            paketNum = 0
+            for item in my_list:
+                interpreted_data = interpretation(item)
+                if interpreted_data is not None:
+                    # Add interpreted data to the thread-safe queue
+                    data_queue.put(interpreted_data)
+            my_list = []
+
+
+# Function to insert data into the database in a separate thread
+def db_worker():
+    while True:
+        # Get data from the queue and insert into the database
+        data = data_queue.get()
+        if data is None:
+            break  # Sentinel for exit
+        insertToDB("BatteryManagement", "Cluster", {"data": data})
+        data_queue.task_done()
+        print("Data inserted into DB:", data)
+
+
+def main():
+    # Set up serial connection (adjust parameters as needed)
+    ser = serial.Serial(
+        port="/dev/serial0",  # Update with your serial port
+        baudrate=56000,
+        timeout=0  # Non-blocking read
+    )
+
+    # Create and start threads
+    threading.Thread(target=read_serial, args=(ser,), daemon=True).start()
+    db_thread = threading.Thread(target=db_worker, daemon=True)
+    db_thread.start()
+
+    try:
+        # Main thread can perform other tasks or just wait for threads to finish
+        while True:
+            time.sleep(1)  # Simulate some work (e.g., logging, monitoring)
+    except KeyboardInterrupt:
+        print("Exiting program.")
+    finally:
+        # Clean up
+        ser.close()
+        # Insert 'None' as a sentinel to shut down db_worker thread
+        data_queue.put(None)
+        db_thread.join()
+
+
+if __name__ == '__main__':
+    main()
